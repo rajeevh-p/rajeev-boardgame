3eaa6408a1c55f50c48a79975ecd23564322cb32
rajeevh-p <rajeevgowda912002@gmail.com>
2026-08-28 19:11:09 +0530

    Extend DatabaseServiceProjectApplication for servlet support

--- src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
+++ src/main/java/com/javaproject/DatabaseServiceProjectApplication.java
@@ -2,11 +2,18 @@ package com.javaproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DatabaseServiceProjectApplication {
+public class DatabaseServiceProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatabaseServiceProjectApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DatabaseServiceProjectApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseServiceProjectApplication.class, args);
+    }
 }
